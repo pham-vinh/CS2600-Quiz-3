1a25aff74dc52d2b3c20c1971c0c660c1270dc1d
random-debug <112684637+pham-vinh@users.noreply.github.com>
2022-10-19 20:02:56 -0700

    Transferred from a different repo

---
+++ EmployeeSearch/employeeMain.c
@@ -0,0 +1,54 @@
+// Compile using the following command: gcc employeeMain.c employeeTable.c employeeOne.c
+// gcc employeeMain.c employeeTable.c employeeTwo.c
+
+#include <string.h>
+#include <stdlib.h>
+#include "employee.h"
+
+int main(void)
+{
+	// defined in employeeSearchOne.c
+	// in employeeOne.c
+	PtrToEmployee searchEmployeeByNumber(const Employee table[], int sizeTable, long numberToFind);
+	PtrToEmployee searchEmployeeByName(const Employee table[], int sizeTable, char *nameToFind);
+	PtrToEmployee searchEmployeeByPhoneNumber(const Employee table[], int sizeTable, char *phoneToFind);
+	PtrToEmployee searchEmployeeBySalary(const Employee table[], int sizeTable, double salaryToFind);
+
+	// defined in employeeTable.c
+	// Defined somewhere else
+	extern Employee EmployeeTable[];
+	extern const int EmployeeTableEntries;
+
+	PtrToEmployee matchPtr; // Declaration
+	matchPtr = searchEmployeeByNumber(EmployeeTable, EmployeeTableEntries, 1045);
+
+	// Example not found
+	if (matchPtr != NULL)
+		printf("Employee ID 1045 is in record %d\n", matchPtr - EmployeeTable);
+	else
+		printf("Employee ID is NOT found in the record\n");
+
+	// Example found
+	matchPtr = searchEmployeeByName(EmployeeTable, EmployeeTableEntries, "Tony Bobcat");
+
+	if (matchPtr != NULL)
+		printf("Employee Tony Bobcat is in record %d\n", matchPtr - EmployeeTable);
+	else
+		printf("Employee Tony Bobcat is NOT found in the record\n");
+
+	matchPtr = searchEmployeeByPhoneNumber(EmployeeTable, EmployeeTableEntries, "310-555-1215");
+
+	if (matchPtr != NULL)
+		printf("Employee Dorris Perl is in record %d\n", matchPtr - EmployeeTable);
+	else
+		printf("Employee Dorris Perl is NOT found in the record\n");
+
+	matchPtr = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, 8.32);
+
+	if (matchPtr != NULL)
+		printf("Employee Brian Height is in record %d\n", matchPtr - EmployeeTable);
+	else
+		printf("Employee Brian Height is NOT found in the record\n");
+
+	return EXIT_SUCCESS;
+}
