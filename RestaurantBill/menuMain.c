de7bf4733493cb52917b03ea28ea554cd151f080
random-debug <112684637+pham-vinh@users.noreply.github.com>
2022-10-19 22:53:09 -0700

    created an outline for Restaurant Bill

---
+++ RestaurantBill/menuMain.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+
+	// initiaize variable for random.
+
+
+	// create an array of menu items
+
+	// take in command line arguements for tip, and tax percentages
+
+    /* Init random number generator using time */
+
+    /* Loop through array of menu items and assign random price to menu item */
+
+
+	// call function to display [meal cost, tax amount, and total bill]
+	// have a function to add the total bill including tip
+	
+    return EXIT_SUCCESS;
+}
