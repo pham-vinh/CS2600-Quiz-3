ab3b4fc04af159a588f0e7cee25a9080bf9d26ff
random-debug <112684637+pham-vinh@users.noreply.github.com>
2022-10-20 13:18:55 -0700

    Outline in main

---
+++ LoShuMagicSquare/main.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// function to print the square
+
+// function to fill the square with random numbers using time
+
+// function to check if it is a magic square
+
+
+int main() {
+
+    // initiate a 2 x 2.. 2d array
+
+    // do while loop while the function magic square is not true
+
+    // have a counter for the number of squares generated
+
+
+
+    // at the end print the grid
+
+
+    return EXIT_SUCCESS;
+}
