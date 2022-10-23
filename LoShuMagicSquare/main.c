340511810259da93869ed783b02807ebdbaafa84
random-debug <112684637+pham-vinh@users.noreply.github.com>
2022-10-22 21:42:16 -0700

    Fixed Output

--- LoShuMagicSquare/main.c
+++ LoShuMagicSquare/main.c
@@ -9,11 +9,12 @@ void printSquare(int square[3][3])
 
     for (int i = 0; i < 3; i++)
     {
+        printf("[ ");
         for (int j = 0; j < 3; j++)
         {
             printf("%d ", square[i][j]);
         }
-        printf("\n");
+        printf("]\n");
     }
 }
 
